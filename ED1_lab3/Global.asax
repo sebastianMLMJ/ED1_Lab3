5f3df3600aa4041c3829d0b25467d48095809565
estudiante <estudiante@T204-15.laburl>
2019-03-21 08:16:10 -0600

    Proyecto vacio

---
+++ ED1_lab3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ED1_lab3.MvcApplication" Language="C#" %>
---
+++ ED1_lab3/Models/Modelo1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ED1_lab3.Models
+{
+    public class Modelo1
+    {
+    }
+}
